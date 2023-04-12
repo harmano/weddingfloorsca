19a2f2883f6050ad76ae23cc33d6223de18d0459
Harman <harman@Harmans-iMac.local>
2023-04-12 14:02:28 -0400

    added first

---
+++ pricing.js
@@ -0,0 +1,215 @@
+/*........................Other......................*/
+var removal = "$100.00";
+
+
+/*........................BASIC DECAL......................*/
+/* DECAL BASIC BLACK */
+var BSfourbyFourBlk = "400.00";
+var BSsixBysixBlk = "$500.00";
+var BSeightByeightBlk = "$600.00";
+var BStenBytenBlk = "$700.00";
+var BStwelveBytwelveBlk = "$800.00";
+
+var BSfourByfourBlkCircle = "400.00";
+var BSsixBysixBlkCircle = "$500.00";
+var BSeightByeightBlkCircle = "$600.00";
+var BStenBytenBlkCircle = "$700.00";
+var BStwelveBytwelveBlkCircle = "$800.00";
+
+var BSfourByfourBlkDate = "400.00";
+var BSsixBysixBlkDate = "$500.00";
+var BSeightByeightBlkDate = "$600.00";
+var BStenBytenBlkDate = "$700.00";
+var BStwelveBytwelveBlkDate = "$800.00";
+
+/* DECAL BASIC COLOR */
+var BSfourByfourC = "C4500.00";
+var BSsixBysixC = "C$5500.00";
+var BSeightByeightC = "C$6500.00";
+var BStenBytenC = "C$7500.00";
+var BStwelveBytwelveC = "C$8050.00";
+
+var BSfourByfourCDate = "C$4500.00";
+var BSsixBysixCDate = "C$5500.00";
+var BSeightByeightCDate = "C$6500.00";
+var BStenBytenCDate = "C$7500.00";
+var BStwelveBytwelveCDate = "C$8050.00";
+
+var BSfourByfourCCircle = "C$4500.00";
+var BSsixBysixCCircle = "C$5500.00";
+var BSeightByeightCCircle = "C$6500.00";
+var BStenBytenCCircle = "C$7500.00";
+var BStwelveBytwelveCCircle = "C$8050.00";
+
+/* DECAL BASIC GOLD */
+var BSfourByfourG = "G400.00";
+var BSsixBysixG = "G500.00";
+var BSeightByeightG = "G$600.00";
+var BStenBytenG = "G700.00";
+var BStwelveBytwelveG = "G800.00";
+
+var BSfourByfourGDate = "G400.00";
+var BSsixBysixGDate = "G500.00";
+var BSeightByeightGDate = "G$600.00";
+var BStenBytenGDate = "G700.00";
+var BStwelveBytwelveGDate = "G800.00";
+
+
+var BSfourByfourGCircle = "G400.00";
+var BSsixBysixGCircle = "G500.00";
+var BSeightByeightGCircle = "G$600.00";
+var BStenBytenGCircle = "G700.00";
+var BStwelveBytwelveGCircle = "G800.00";
+
+
+/*........................STANDARD DECAL......................*/
+/* DECAL STANDARD BLACK */
+var DSfourByfourBlk = "400.00";
+var DSsixBysixBlk = "$500.00";
+var DSeightByeightBlk = "$600.00";
+var DStenBytenBlk = "$700.00";
+var DStwelveBytwelveBlk = "$800.00";
+
+var DSfourByfourBlkCircle = "400.00Circle";
+var DSsixBysixBlkCircle = "$500.00Circle";
+var DSeightByeightBlkCircle = "$600.00Circle";
+var DStenBytenBlkCircle = "$700.00Circle";
+var DStwelveBytwelveBlkCircle = "$800.00Circle";
+
+var DSfourByfourBlkDate = "400.00Date";
+var DSsixBysixBlkDate = "$500.00Date";
+var DSeightByeightBlkDate = "$600.00Date";
+var DStenBytenBlkDate = "$700.00Date";
+var DStwelveBytwelveBlkDate = "$800.00Date";
+
+/* DECAL STANDARD COLOR */
+var DSfourByfourC = "C4500.00";
+var DSsixBysixC = "C$5500.00";
+var DSeightByeightC = "C$6500.00";
+var DStenBytenC = "C$7500.00";
+var DStwelveBytwelveC = "C$8050.00";
+
+var DSfourByfourCCircle = "C4500.00Circle";
+var DSsixBysixCCircle = "C$5500.00Circle";
+var DSeightByeightCCircle = "C$6500.00Circle";
+var DStenBytenCCircle = "C$7500.00Circle";
+var DStwelveBytwelveCCircle = "C$8050.00Circle";
+
+var DSfourByfourCDate = "C4500.00Date";
+var DSsixBysixCDate = "C$5500.00Date";
+var DSeightByeightCDate = "C$6500.00Date";
+var DStenBytenCDate = "C$7500.00Date";
+var DStwelveBytwelveCDate = "C$8050";
+
+/* DECAL STANDARD GOLD */
+var DSfourByfourG = "G400.00";
+var DSsixBysixG = "G500.00";
+var DSeightByeightG = "G$600.00";
+var DStenBytenG = "G700.00";
+var DStwelveBytwelveG = "G800.00";
+
+var DSfourByfourGDate = "G400.00";
+var DSsixBysixGDate = "G500.00";
+var DSeightByeightGDate = "G$600.00";
+var DStenBytenGDate = "G700.00";
+var DStwelveBytwelveGDate = "G800.00";
+
+var DSfourByfourGCircle = "G400.00Circle";
+var DSsixBysixGCircle = "G500.00Circle";
+var DSeightByeightGCircle = "G$600.00Circle";
+var DStenBytenGCircle = "G700.00Circle";
+var DStwelveBytwelveGCircle = "G800.00Circle";
+
+/*........................PREMIUM DECAL......................*/
+/* DECAL STANDARD BLACK */
+var PMfourByfourBlk = "400.00";
+var PMsixBysixBlk = "$500.00";
+var PMeightByeightBlk = "$600.00";
+var PMtenBytenBlk = "$700.00";
+var PMtwelveBytwelveBlk = "$800.00";
+
+var PMfourByfourBlkDate = "400.00";
+var PMsixBysixBlkDate = "$500.00";
+var PMeightByeightBlkDate = "$600.00";
+var PMtenBytenBlkDate = "$700.00";
+var PMtwelveBytwelveBlkDate = "$800.00";
+
+var PMfourByfourCCircle = "C4500.00Circle";
+var PMsixBysixCCircle = "C$5500.00Circle";
+var PMeightByeightCCircle = "C$6500.00Circle";
+var PMtenBytenCCircle = "C$7500.00Circle";
+var PMtwelveBytwelveCCircle = "C$8050.00Circle";
+
+
+/* DECAL STANDARD COLOR */
+var PMfourByfourC = "C4500.00";
+var PMsixBysixC = "C$5500.00";
+var PMeightByeightC = "C$6500.00";
+var PMtenBytenC = "C$7500.00";
+var PMtwelveBytwelveC = "C$8050.00";
+
+var PMfourByfourCCircle = "C4500.00Circle";
+var PMsixBysixCCircle = "C$5500.00Circle";
+var PMeightByeightCCircle = "C$6500.00Circle";
+var PMtenBytenCCircle = "C$7500.00Circle";
+var PMtwelveBytwelveCCircle = "C$8050.00Circle";
+
+var PMfourByfourCDate = "C4500.00";
+var PMsixBysixCDate = "C$5500.00";
+var PMeightByeightCDate = "C$6500.00";
+var PMtenBytenCDate = "C$7500.00";
+var PMtwelveBytwelveCDate = "C$8050.00";
+
+/* DECAL STANDARD GOLD */
+var PMfourByfourG = "G400.00";
+var PMsixBysixG = "G500.00";
+var PMeightByeightG = "G$600.00";
+var PMtenBytenG = "G700.00";
+var PMtwelveBytwelveG = "G800.00";
+
+var PMfourByfourGCircle = "G400.00Circle";
+var PMsixBysixGCircle = "G500.00Circle";
+var PMeightByeightGCircle = "G$600.00Circle";
+var PMtenBytenGCircle = "G700.00Circle";
+var PMtwelveBytwelveGCircle = "G800.00Circle";
+
+var PMfourByfourGDate = "G400.00Circle";
+var PMsixBysixGDate = "G500.00Circle";
+var PMeightByeightGDate = "G$600.00Circle";
+var PMtenBytenGDate = "G700.00Circle";
+var PMtwelveBytwelveGDate = "G800.00Circle";
+
+
+const classes = [
+    "removal", 
+    "BSfourbyFourBlk", 
+    "BSsixBysixBlk",
+     "BSeightByeightBlk",
+      "BStenBytenBlk", 
+      "BStwelveBytwelveBlk", 
+      "BSfourByfourBlkCircle", 
+      "BSsixBysixBlkCircle", 
+      "BSeightByeightBlkCircle", 
+      "BStenBytenBlkCircle", 
+      "BStwelveBytwelveBlkCircle", 
+    "BSfourByfourBlkDate", 
+    "BSsixBysixBlkDate", 
+    "BSeightByeightBlkDate", 
+    "BStenBytenBlkDate", 
+    "BStwelveBytwelveBlkDate", 
+    "BSfourByfourC", 
+    "BSsixBysixC", 
+    "BSeightByeightC", 
+    "BStenBytenC", 
+    "BStwelveBytwelveC", 
+    "BSfourByfourCDate", "BSsixBysixCDate", "BSeightByeightCDate", "BStenBytenCDate", "BStwelveBytwelveCDate", "BSfourByfourCCircle", "BSsixBysixCCircle", "BSeightByeightCCircle", "BStenBytenCCircle", "BStwelveBytwelveCCircle", "BSfourByfourG", "BSsixBysixG", "BSeightByeightG", "BStenBytenG", "BStwelveBytwelveG", "BSfourByfourGDate", "BSsixBysixGDate", "BSeightByeightGDate", "BStenBytenGDate", "BStwelveBytwelveGDate", "BSfourByfourGCircle", "BSsixBysixGCircle", "BSeightByeightGCircle", "BStenBytenGCircle", "BStwelveBytwelveGCircle", "DSfourByfourBlk", "DSsixBysixBlk", "DSeightByeightBlk", "DStenBytenBlk", "DStwelveBytwelveBlk", "DSfourByfourBlkCircle", "DSsixBysixBlkCircle", "DSeightByeightBlkCircle", "DStenBytenBlkCircle", "DStwelveBytwelveBlkCircle", "DSfourByfourBlkDate", "DSsixBysixBlkDate", "DSeightByeightBlkDate", "DStenBytenBlkDate", "DStwelveBytwelveBlkDate", "DSfourByfourC", "DSsixBysixC", "DSeightByeightC", "DStenBytenC", "DStwelveBytwelveC", "DSfourByfourCCircle", "DSsixBysixCCircle", "DSeightByeightCCircle", "DStenBytenCCircle", "DStwelveBytwelveCCircle", "DSfourByfourCDate", "DSsixBysixCDate", "DSeightByeightCDate", "DStenBytenCDate", "DStwelveBytwelveCDate", "DSfourByfourG", "DSsixBysixG", "DSeightByeightG", "DStenBytenG", "DStwelveBytwelveG", "DSfourByfourGDate", "DSsixBysixGDate", 
+    "DSeightByeightGDate", 
+  ];
+  
+  classes.forEach((className) => {
+    const elements = document.getElementsByClassName(className);
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].innerHTML = window[className];
+    }
+  });
+  
